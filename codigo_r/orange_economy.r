4082384f85568442590c6c64f8e4c3515feeb36e
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2021-06-27 19:27:26 -0500

    Adding basic vactors and data operation

--- codigo_r/orange_economy.r
+++ codigo_r/orange_economy.r
@@ -1,3 +1,34 @@
 
-
+#Opening File
 str(orange_economy_latam)
+
+#Basic Statistics
+summary(orange_economy_latam)
+
+summary(mtcars)
+
+wt <- (mtcars$wt*1000)/2
+wt
+
+summary(mtcars)
+
+#Data transformation
+
+mtcars.new <- transform(mtcars,wt=wt*1000/2)
+
+mtcars.new
+
+summary(mtcars.new)
+
+#Vectores
+
+tiempo_platzi <- c(25,5,10,15,10)
+tiempo_platzi
+
+tiempo_lecturas <- c(30,10,5,10,15)
+tiempo_lecturas
+
+tiempo_aprendizaje <- tiempo_platzi + tiempo_lecturas
+tiempo_aprendizaje
+
+
